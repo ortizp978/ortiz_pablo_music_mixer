db683e0a77e0a948aa5fb435a4a6fe056804e73f
Pablo Ortiz <pablortiz78@MacBook-Pro-de-Pablo.local>
2022-04-05 10:09:44 -0400

    finishing project

--- js/main.js
+++ js/main.js
@@ -1,37 +1,56 @@
 (() => {
   let instrumentsBoard = document.querySelector(".generalDropZone"),
       instruments = document.querySelectorAll("#generalDragZone *"),
-		  dropZones = document.querySelectorAll(".dropzone");
+      dropZones = document.querySelectorAll(".dropZone"),
+		  dropZonesOut = document.querySelectorAll(".containerExtern");
 
-  const instrumentsPaths = ["guitar", "bass", "drum", "maracas"]
+  const instrumentsPaths = ["guitar", "bass", "drum", "maracas"];
+  var audio = new Array();
+  audio["guitar"] = new Audio("assets/guitar.mp3");
+  audio["bass"] = new Audio("assets/bass.mp3");
+  audio["drum"] = new Audio("assets/drum.mp3");
+  audio["maracas"] = new Audio("assets/maracas.mp3");
 
   function dragStarted(event) {
-		console.log('started draggin a piece');
 		event.dataTransfer.setData('currentItem', event.target.id);
 	}
 
   function allowDragOver(event) {
     event.preventDefault();
-    console.log('dragged over me');
   }
 
   function allowDrop(event) {
     event.preventDefault();
-    console.log('dropped on me');
-
-    //if (detectImg(event.currentTarget.className)== false){return;};
-
     let droppedEl = event.dataTransfer.getData('currentItem');
-    console.log(droppedEl);
+    this.appendChild(document.querySelector(`#${droppedEl}`));
+    playAudio(droppedEl);
+  }
 
+  function allowDropOut(event) {
+    event.preventDefault();
+    let droppedEl = event.dataTransfer.getData('currentItem');
+    stopAudio(droppedEl);
     this.appendChild(document.querySelector(`#${droppedEl}`));
   }
 
-   instruments.forEach(piece => piece.addEventListener ("dragstart", dragStarted));
+  function playAudio(instrumentSel){
+    audio[instrumentSel].loop= true;
+    audio[instrumentSel].play();
+  }
 
-   dropZones.forEach(zone => {
+  function stopAudio(instrumentSel){
+    audio[instrumentSel].pause();
+  }
+
+  instruments.forEach(piece => piece.addEventListener ("dragstart", dragStarted));
+
+  dropZones.forEach(zone => {
      zone.addEventListener("dragover", allowDragOver);
      zone.addEventListener("drop", allowDrop);
-   });
+  });
 
-  })();
+  dropZonesOut.forEach(zone => {
+     zone.addEventListener("drop", allowDropOut);
+  });
+
+})();
